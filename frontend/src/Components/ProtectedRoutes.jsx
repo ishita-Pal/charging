453f9de9903289c492454e733d5d1b3a0d95e49a
ishita <ishitapal212002@gmail.com>
2025-06-03 08:28:32 +0530

    delte

--- frontend/src/Components/ProtectedRoutes.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom';
-import { isAuthenticated } from '../services/auth';
-
-const ProtectedRoute = () => {
-  return isAuthenticated() ? <Outlet /> : <Navigate to="/" replace />;
-};
-
-export default ProtectedRoute;
